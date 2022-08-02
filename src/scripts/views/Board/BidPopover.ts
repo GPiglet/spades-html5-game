e25d75f6eae227fd23849dd901a2a1a212c6748d
Piglet <piglet20181112@outlook.com>
2022-08-02 00:11:06 -0700

    show, hide, setMaxLevel, selectBidHandler, z-index

--- src/scripts/views/Board/BidPopover.ts
+++ src/scripts/views/Board/BidPopover.ts
@@ -1,26 +1,26 @@
-// <div id="bid-div" class="absolute z-40 w-full bottom-[290px] text-center">
-//     <div id="bid-div-inner" class="bg-tip inline-block z-40 rounded-lg border-[1px] border-black p-1">
-//         <h4 class="text-[#800000] text-center z-10">What's your bid?</h4>
-//         <span>0</span><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span><span>7</span><span>8</span><span>9</span><span>10</span><span>11</span><span>12</span><span>13</span>
-//     </div>
-// </div>
+import gsap from "gsap";
+
 class BidPopover {
   container: HTMLElement;
-  constructor(parent: HTMLElement | null) {
+  buttons: Array<HTMLElement> = [];
+  onSelectBid: any = null;
+  constructor(parent: HTMLElement | null, onSelectBid: any = null) {
+    this.onSelectBid = onSelectBid;
     this.container = document.createElement('div');
     this.container.id = 'bid-div';
     parent?.appendChild(this.container);
     this.container.classList.add('absolute');
-    this.container.classList.add('z-40');
     this.container.classList.add('w-full');
+    this.container.classList.add('sm:w-4/5');
     this.container.classList.add('bottom-[290px]');
     this.container.classList.add('text-center');
+    this.container.classList.add('sm:left-[70px]');
+    this.container.style.zIndex = '60';
 
     const wrapper = document.createElement('div');
     this.container.appendChild(wrapper);
     wrapper.classList.add('bg-tip');
     wrapper.classList.add('inline-block');
-    wrapper.classList.add('z-40');
     wrapper.classList.add('rounded-lg');
     wrapper.classList.add('border-[1px]');
     wrapper.classList.add('border-black');
@@ -36,7 +36,41 @@ class BidPopover {
       const button = document.createElement('span');
       button.innerHTML = '' + i;
       wrapper.appendChild(button);
+      this.buttons.push(button);
+      button.onclick = (e: MouseEvent) => {
+        this.onClickBid(i);
+      };
     }
+
+    this.container.style.visibility = 'hidden';
+  }
+
+  onClickBid(level: number) {
+    if ( this.onSelectBid ) this.onSelectBid(level);
+    this.hide();
+  }
+
+  show(maxLevel: number = 13) {
+    this.setBidLevel(maxLevel);
+    gsap.set(this.container, {opacity: 0});
+    gsap.to(this.container, {opacity: 1, onComplete: () => {
+      this.container.style.visibility = 'visible';
+    }})
+  }
+
+  hide() {
+    gsap.to(this.container, {opacity: 0, onComplete: () => {
+      this.container.style.visibility = 'hidden';
+    }})
+  }
+
+  setBidLevel(maxLevel: number) {
+    this.buttons.forEach( (btn, i) => {
+      if ( i > maxLevel )
+        btn.style.display = 'none';
+      else
+        btn.style.display = 'inline-block';
+    })
   }
 }
 
