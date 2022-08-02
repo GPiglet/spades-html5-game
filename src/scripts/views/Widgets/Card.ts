58f329dddfc60c6fc784d8f0da5d579f0c70765c
Piglet <piglet20181112@outlook.com>
2022-08-01 22:23:36 -0700

    add cardType property

--- src/scripts/views/Widgets/Card.ts
+++ src/scripts/views/Widgets/Card.ts
@@ -1,6 +1,10 @@
+import { CardType } from "../Board/CardFactory";
+
 class Card {
   container: HTMLElement;
-  constructor(classList: Array<string>) {
+  cardType: CardType;
+  constructor(cardType: CardType, classList: Array<string>) {
+    this.cardType = cardType;
     this.container = document.createElement('div');
     classList.forEach((className) => {
       this.container.classList.add(className);
