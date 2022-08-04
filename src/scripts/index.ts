1bfd41afafb1e192b576d5ed9e58fd2ff3556c36
Piglet <piglet20181112@outlook.com>
2022-08-04 11:31:02 -0700

    onload

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,37 +1,30 @@
 import '../styles/styles.css';
 import Player from './views/Player';
-import CardFactory, { CardType } from './views/Board/CardFactory';
-import BidPopover from './views/Board/BidPopover';
 import PlayGround from './views/Board';
 import DealGround from './views/Board/DealGround';
-import TestController from './controllers/test';
 import testController from './controllers/test';
+import mainController from './controllers/main';
 
-// root element
-const rootElement = document.getElementById('root');
+window.addEventListener('load', () => {
+  // root element
+  const rootElement = document.getElementById('root');
 
-// init players
-const players: Array<Player> = [];
-players.push(new Player(rootElement, 'top', 'Bill', 'assets/images/face-2.svg'));
-players.push(new Player(rootElement, 'right', 'Lisa', 'assets/images/face-3.svg'));
-players.push(new Player(rootElement, 'bottom', 'You', 'assets/images/face-0.svg'));
-players.push(new Player(rootElement, 'left', 'Mike', 'assets/images/face-mike.svg'));
+  // init players
+  const players: Array<Player> = [];
+  players.push(new Player(rootElement, 'top', 'Bill', 'assets/images/face-2.svg'));
+  players.push(new Player(rootElement, 'right', 'Lisa', 'assets/images/face-3.svg'));
+  players.push(new Player(rootElement, 'bottom', 'You', 'assets/images/face-0.svg'));
+  players.push(new Player(rootElement, 'left', 'Mike', 'assets/images/face-mike.svg'));
 
-// init deal ground
-const dealGround = new DealGround(rootElement, players);
-dealGround.init().show();
+  // init deal ground
+  const dealGround = new DealGround(rootElement, players);
+  mainController.setDealGround(dealGround);
+  dealGround.init().show();
 
-// enable test mode
-testController.enableTestMode();
+  // init play ground
+  const playGround = new PlayGround(rootElement, players);
 
-// leftPlayer.addCard(CardFactory.create(CardType.Blank, 'left'));
-// leftPlayer.addCard(CardFactory.create(CardType.Blank, 'left'));
-// leftPlayer.addCard(CardFactory.create(CardType.Blank, 'left'));
-// leftPlayer.addCard(CardFactory.create(CardType.Blank, 'left'));
-// leftPlayer.addCard(CardFactory.create(CardType.Blank, 'left'));
+  // enable test mode
+  testController.enableTestMode();
 
-// const playGround = new PlayGround(rootElement);
-// playGround.putCard(CardFactory.create(CardType.Blank, 'bottom'), 'top');
-// playGround.putCard(CardFactory.create(CardType.Blank, 'bottom'), 'right');
-// playGround.putCard(CardFactory.create(CardType.Blank, 'bottom'), 'bottom');
-// playGround.putCard(CardFactory.create(CardType.Blank, 'bottom'), 'left');
+})
